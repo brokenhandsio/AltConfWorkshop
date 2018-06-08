1047e64878c45c66f0c5dea37883511c1aa1cf52
Tim <0xtimc@gmail.com>
2018-06-08 10:30:29 -0700

    Add code from workshop

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,17 @@
+import FluentSQLite
+import Vapor
+
+final class User: Codable {
+    var id: Int?
+    var name: String
+    var username: String
+
+    init(name: String, username: String) {
+        self.name = name
+        self.username = username
+    }
+}
+
+extension User: SQLiteModel {}
+extension User: Content {}
+extension User: Migration {}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,4 +12,44 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+
+    router.get("hello", "altconf") { req in
+        return "Hello Alt Conf!"
+    }
+
+    router.get("bottles", Int.parameter) { req -> Bottles in
+        let count = try req.parameters.next(Int.self)
+        return Bottles(count: count)
+    }
+
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello \(name)"
+    }
+
+    router.post(Bottles.self, at: "bottles") { req, bottles -> String in
+        return "There were \(bottles.count) bottles"
+    }
+
+    router.post(UserInfoData.self, at: "user-info") { req, data -> UserInfo in
+        let message = "Hello \(data.name), you are \(data.age)"
+        return UserInfo(message: message)
+    }
+
+    let usersController = UsersController()
+    try router.register(collection: usersController)
 }
+
+struct Bottles: Content {
+    let count: Int
+}
+
+struct UserInfoData: Content {
+    let name: String
+    let age: Int
+}
+
+struct UserInfo: Content {
+    let message: String
+}
+
